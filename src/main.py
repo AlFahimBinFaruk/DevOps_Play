d81d8c4961fdccace4a0fe9e84eacb8e55befd71
AlFahimBinFaruk <alfahim.ps@gmail.com>
2025-09-30 18:54:24 +0600

    configured prometheous,loki,grafana

--- src/main.py
+++ src/main.py
@@ -3,7 +3,26 @@ from .db.database import lifespan
 import socket
 import os
 
+from prometheus_fastapi_instrumentator import Instrumentator
+import logging
+from logging_loki import LokiHandler
+
+
+# Loki handler
+logger = logging.getLogger("fastapi")
+logger.setLevel(logging.INFO)
+loki_handler = LokiHandler(
+    url="http://loki:3100/loki/api/v1/push",
+    tags={"application": "fastapi_app"},
+    version="1",
+)
+logger.addHandler(loki_handler)
+
+# Initialize app.
 app = FastAPI(lifespan=lifespan)
+
+# Prometheus Instrumentator
+Instrumentator().instrument(app).expose(app)
 
 from .user.views import router as user_router
 from .todo.views import router as todo_router
@@ -14,7 +33,9 @@ app.include_router(todo_router)
 
 @app.get("/")
 async def read_root():
+    logger.info("Root endpoint was accessed.")
     return {"message": "Server Running!"}
+
 
 @app.get("/info")
 async def container_info():
@@ -23,5 +44,5 @@ async def container_info():
         "container_id": socket.gethostname(),
         "container_name": os.environ.get("HOSTNAME", "unknown"),
         "process_id": os.getpid(),
-        "message": f"Response from container: {socket.gethostname()}"
+        "message": f"Response from container: {socket.gethostname()}",
     }
